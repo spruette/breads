38b7943457902861c663978aac7a4e57b28c63ae
unknown <scotteychaos@gmail.com>
2022-08-18 18:28:22 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -6,8 +6,16 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread
+    }
+  )
+// res.send(Bread)
 })
+
+
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
