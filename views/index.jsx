f276b92bd798b97bdc5da107bda80b64277a94dd
spruette <103010101+spruette@users.noreply.github.com>
2022-08-21 09:51:53 -0400

    helper method pt 1 complete

--- views/index.jsx
+++ views/index.jsx
@@ -1,36 +1,29 @@
-const React = require('react')
-const Default = require('./layouts/default')
+const React = require("react");
+const Default = require("./layouts/Default");
 
-function Index ({breads, title})  {
-    return (
-        <Default title={title}>
-        <h2>Index Page</h2>
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        {/* This is a JSX comment. */}
-        <ul>
-        {
-  breads.map((bread, index)=> {
-    return (
-      <li key={index}>
-        <a href={`/breads/${index}`}>
-          {bread.name}
+function Index({ breads, title }) {
+  return (
+    <Default title={title}>
+      <h2>Index Page</h2>
+      <div className="newButton">
+        <a href="/breads/new">
+          <button>Add a new bread</button>
         </a>
-      </li>
-    )
-  })
+      </div>
+
+      {/* <p>I have {breads[0].name} bread!</p> */}
+      {/* This is a JSX comment. */}
+      <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key={index}>
+              <a href={`/breads/${bread.id}`}>{bread.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </Default>
+  );
 }
 
-
-
-        </ul>
-        <div className="newButton">
-  <a href="/breads/new"><button>Add a new bread</button></a>
-</div>
-
-      </Default>
-      
-    )
-}
-
-
-module.exports = Index
+module.exports = Index;
